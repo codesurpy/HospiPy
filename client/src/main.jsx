21894c751fab9cb64e03e2513716726543a47b4e
Ruben Amarilla <daniel.amarilla.rda@gmail.com>
2023-06-13 09:25:27 -0400

    feat: Se agrega routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
 import './css/index.css'
+import router from './routes/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
