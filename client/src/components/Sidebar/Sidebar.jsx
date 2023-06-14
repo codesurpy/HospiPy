645b3f198105400d38419d61b7dc97ac7ce2f137
Brihan Penayo <brihan.penayos@gmail.com>
2023-06-14 11:48:17 -0400

    feat: Instalacion y configuracion necesaria para TailwindCSS

---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <section className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}>
+            <aside>
+<h1>TEst</h1>
+            </aside>
+        </section>
+    )
+}
+
+export default Sidebar
