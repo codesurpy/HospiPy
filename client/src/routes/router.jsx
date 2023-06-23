e232cef4030050713d1d4fb62f0eb4bcd2a0d86b
Brihan Penayo <brihan.penayos@gmail.com>
2023-06-23 17:40:27 -0400

    feat: Agregando Layout y Maquetando esqueleto de Sidebar, fix: Altura de imagen en login

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Login from '../pages/Login'
+import { Layout } from '../layout/Layout'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Login />} />
+  createRoutesFromElements([
+    <Route path='/' element={<Login />} />,
+    <Route path='/main' element={<Layout />}>
+      {/*Aqui deben ir los hijos de layout*/}
+    </Route>
+  ]
   )
 )
 
