a0432842f8f2e69dbc3aa35865afc50e31ad5021
derlis14 <derlis@rugertek.com>
2023-07-03 18:59:25 -0400

    fix/Se agrega env de referencia

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
 import Login from '../pages/Login'
 import { Layout } from '../layout/Layout'
+import Dashboard from '../components/Dashboard/Dashboard'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
+    <Route path='/dashboard' element={<Dashboard />} />,
     <Route path='/' element={<Login />} />,
     <Route path='/main' element={<Layout />}>
       {/*Aqui deben ir los hijos de layout*/}
     </Route>
-  ]
-  )
+  ])
 )
 
 export default router
